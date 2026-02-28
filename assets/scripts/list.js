8150c4e85d6b8262c0e14adfaf696d50942f84be
Atom <kevintran308@gmail.com>
2026-02-28 16:28:48 +0700

    the list is now works but it looks ass af

--- assets/scripts/list.js
+++ assets/scripts/list.js
@@ -1,11 +1,39 @@
 fetch("../resources/modes.json")
   .then(response => response.json())
   .then(data => {
-    const listElement = document.querySelector("main ol");
-    data.forEach(mode => {
-      const listItem = document.createElement("li");
-      listItem.textContent = `${mode.name} - ${mode.game}`;
-      listElement.appendChild(listItem);
+    const container = document.getElementById("modesContainer");
+
+    data.forEach((mode, index) => {
+      const card = document.createElement("div");
+      card.className = "card mb-3 p-3 shadow-sm";
+
+      card.innerHTML = `
+        <div class="card">
+          <div class="row g-0">
+            <div class="col-md-4">
+            <a href="https://www.youtube.com/watch?v=${mode.verifierLink}">
+              <img src="https://img.youtube.com/vi/${mode.verifierLink}/maxresdefault.jpg" class="img-fluid rounded-start" alt="...">
+            </a>
+            </div>
+            <div class="">
+              <div class="card-body">
+                <h5 class="card-title">
+                  <span id="ranking">#${index+1} </span>
+                  <a href="https://www.youtube.com/watch?v=${mode.verifierLink}">${mode.name}</a>
+                </h5>
+                <p class="card-text text-muted">
+                  <a href="${mode.gameLink}">${mode.game}</a>
+                </p>
+                <p class="card-text"><small class="text-body-secondary">Developer: ${mode.developer}</small></p>
+                <p class="card-text"><small class="text-body-secondary">Verifier: ${mode.verifier}</small></p>
+                <p class="card-text"><small class="text-body-secondary">Length: ${mode.length}</small></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      `;
+
+      container.appendChild(card);
     });
   })
   .catch(error => console.error("Error loading JSON:", error));
