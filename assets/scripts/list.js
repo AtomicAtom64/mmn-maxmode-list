27914783fbeac491830345a47bea351eacae10e0
Atom <kevintran308@gmail.com>
2026-03-02 12:44:38 +0700

    updated list page

--- assets/scripts/list.js
+++ assets/scripts/list.js
@@ -5,30 +5,39 @@ fetch("../resources/modes.json")
 
     data.forEach((mode, index) => {
       const card = document.createElement("div");
-      card.className = "card mb-3 p-3 shadow-sm";
-
       card.innerHTML = `
-        <div class="card">
+        <div class="card mb-3 p-2 shadow-sm mx-auto">
           <div class="row g-0">
             <div class="col-md-4">
-            <a href="https://www.youtube.com/watch?v=${mode.verifierLink}">
-              <img src="https://img.youtube.com/vi/${mode.verifierLink}/maxresdefault.jpg" class="img-fluid rounded-start" alt="...">
-            </a>
+              <a href="https://www.youtube.com/watch?v=${mode.verifierLink}">
+                <img 
+                  src="https://img.youtube.com/vi/${mode.verifierLink}/maxresdefault.jpg" 
+                  class="thumbnail img-fluid rounded h-100 object-fit-cover" 
+                  alt="${mode.name}">
+              </a>
             </div>
-            <div class="">
+
+            <div class="col-md-8">
               <div class="card-body">
                 <h5 class="card-title">
-                  <span id="ranking">#${index+1} </span>
-                  <a href="https://www.youtube.com/watch?v=${mode.verifierLink}">${mode.name}</a>
+                  <span class="fw-bold">#${index+1}</span>
+                  <a href="https://www.youtube.com/watch?v=${mode.verifierLink}">
+                    ${mode.name}
+                  </a>
                 </h5>
+
                 <p class="card-text text-muted">
                   <a href="${mode.gameLink}">${mode.game}</a>
                 </p>
-                <p class="card-text"><small class="text-body-secondary">Developer: ${mode.developer}</small></p>
-                <p class="card-text"><small class="text-body-secondary">Verifier: ${mode.verifier}</small></p>
-                <p class="card-text"><small class="text-body-secondary">Length: ${mode.length}</small></p>
+
+                <div class="text-body-secondary small">
+                  <div>Developer: ${mode.developer}</div>
+                  <div>Verifier: ${mode.verifier}</div>
+                  <div>Length: ${mode.length}</div>
+                </div>
               </div>
             </div>
+
           </div>
         </div>
       `;
