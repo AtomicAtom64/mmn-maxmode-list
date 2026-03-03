60527eeec85f34ce06a5cfab5ee334724302056e
Atom <kevintran308@gmail.com>
2026-03-03 12:14:26 +0700

    name changes

--- assets/scripts/list.js
+++ assets/scripts/list.js
@@ -1,4 +1,4 @@
-fetch("../resources/modes.json")
+fetch("../resources/main_list.json")
   .then(response => response.json())
   .then(data => {
     const container = document.getElementById("modesContainer");
