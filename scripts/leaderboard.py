603c14039f7845087c85a88698b4e65e5e0efaa0
Atom <kevintran308@gmail.com>
2026-03-05 10:37:33 +0700

    optimized the script and added rng points to the data

--- scripts/leaderboard.py
+++ scripts/leaderboard.py
@@ -17,6 +17,7 @@ start_time = time.time()
 class Record:
     mode_name: str
     mode_skill_pt: int
+    mode_rng_pt: int
     youtube_link: str
 
 @dataclass
@@ -24,6 +25,7 @@ class Member:
     id: str
     name: str
     total_skill_pt: int
+    total_rng_pt: int
     records: list[Record]
 
 def get_members(session):
@@ -33,19 +35,11 @@ def get_members(session):
         raise Exception(f"Failed to fetch clan members: {r.status_code}")
     
     results = r.json()
-    return [member["user_id"] for member in results]
+    return [member["user_id"] for member in results], [member["players"]["name"] for member in results]
 
-def get_info(session, id):
-    player_url = f"https://aml-api-eta.vercel.app/player/{id}/"
-    
-    r = session.get(player_url, timeout=10)
-    if not r.ok:
-        raise Exception(f"Failed to fetch player info for ID {id}: {r.status_code}")
-    
-    results = r.json()
-    player_name = results["name"].strip()
-    total_skill_pt = int(results["totalSkillpt"] or 0)
-    
+def get_info(session, id, name):
+    player_name = name
+
     modes_url = f"https://aml-api-eta.vercel.app/player/{id}/records/skillValue"
     
     r = session.get(modes_url, timeout=10)
@@ -59,23 +53,27 @@ def get_info(session, id):
         for mode in modes_results:
             mode_name = mode["levels"]["name"]
             skillpt = int(mode.get("skillValue") or 0)
+            rngpt = int(mode.get("rngValue") or 0)
             link = mode["videoLink"]
-            records.append(Record(mode_name, skillpt, link))
+            records.append(Record(mode_name, skillpt, rngpt, link))
+            
+    total_skill_pt = sum(record.mode_skill_pt for record in records)
+    total_rng_pt = sum(record.mode_rng_pt for record in records)
     
-    return Member(id, player_name, total_skill_pt, records)  
+    return Member(id, player_name, total_skill_pt, total_rng_pt, records)
 
 session = requests.Session()
-members_id = get_members(session)
+members_id, members_names = get_members(session)
 
-def fetch(id):
+def fetch(id, name):
     try:
-        return get_info(session, id)
+        return get_info(session, id, name)
     except Exception as e:
         print(f"Error fetching info for ID {id}: {e}")
         return None
 
 with ThreadPoolExecutor(max_workers=5) as executor:
-    members = list(executor.map(fetch, members_id))
+    members = list(executor.map(fetch, members_id, members_names))
     
 members = [m for m in members if m is not None]
 
