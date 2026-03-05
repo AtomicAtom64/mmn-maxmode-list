849b029da7ca89ad205e29a379632d23c5ae40cb
Atom <kevintran308@gmail.com>
2026-03-05 12:34:55 +0700

    leaderboard now has 2 extra sorting types + updated member data

--- assets/scripts/leaderboard.js
+++ assets/scripts/leaderboard.js
@@ -1,49 +1,104 @@
 members = [];
+const failedAvatars = new Set();
+const scoring = {
+    "OG": m => m.computedSkill,
+    "100/0": m => m.total_skill_pt,
+    "90/10": m => m.total_skill_pt * 0.9 + m.total_rng_pt * 0.1
+};
 
 fetch("../resources/members.json")
     .then(response => response.json())
     .then(data => {
 
-        const members = data.map(m => ({
+        members = data.map(m => ({
             ...m,
             computedSkill: getRecords(m.records)
         }));
 
         members.sort((a, b) => b.computedSkill - a.computedSkill);
 
-        const container = document.getElementById("leaderboardContainer");
+        displayLeaderboard(members, "OG");
+    });
 
-        container.innerHTML = members.map((m, index) => `
-            <div class="list-group-item d-flex align-items-center justify-content-between">
-                <div class="d-flex align-items-center gap-3">
-                    <span class="fw-bold fs-5 text-muted" style="width:40px">
-                        #${index + 1}
-                    </span>
-                    <img 
-                        src="${getAvatar(m.id)}"
-                        class="profile rounded-circle"
-                        alt="${m.name}"
-                        onerror="handleAvatarError(this)"
-                    >
-                    <div>
-                        <div class="fw-semibold">${m.name}</div>
-                        <small class="text-muted">
-                            Hardest: ${getHardest(m.records)}
-                        </small>
+function sorting(members, type){
+    const score = scoring[type];
+    members.sort((a, b) => score(b) - score(a));
+}
+
+function displayLeaderboard(members, type) {
+    sorting(members, type);
+    const container = document.getElementById("leaderboardContainer");
+
+    container.innerHTML = members.map((m, index) => {
+
+        let scoreText;
+
+        if (type === "90/10") {
+            scoreText = `
+                <div class="text-end">
+                    <div class="fw-bold text-primary fs-5">
+                        ${(m.total_skill_pt * 0.9 + m.total_rng_pt * 0.1).toFixed(0)} Pts
                     </div>
+                    <small class="text-muted">
+                        ${m.total_skill_pt} Skill | ${m.total_rng_pt} RNG
+                    </small>
                 </div>
+            `;
+        } else if(type === "100/0") {
+            scoreText = `
+                <div class="fw-bold text-primary fs-5">
+                    ${m.total_skill_pt} Pts
+                </div>
+            `;
+        } else {
+            scoreText = `
                 <div class="fw-bold text-primary fs-5">
                     ${m.computedSkill} Pts
                 </div>
+            `;
+        }
+
+        return `
+        <div class="list-group-item d-flex align-items-center justify-content-between">
+            <div class="d-flex align-items-center gap-3">
+                <span class="fw-bold fs-5 text-muted index">
+                    #${index + 1}
+                </span>
+                <img 
+                    src="${getAvatar(m.id)}"
+                    class="profile rounded-circle"
+                    data-id="${m.id}"
+                    alt="${m.name}"
+                    onerror="handleAvatarError(this)"
+                >
+                <div class="player-info">
+                    <div class="fw-semibold">${m.name}</div>
+                    <small class="text-muted hardest">
+                        Hardest: ${getHardest(m.records)}
+                    </small>
+                </div>
             </div>
-        `).join('');
-    });
+
+            ${scoreText}
+
+        </div>
+        `;
+    }).join('');
+}
 
 function getAvatar(id){
+    if (failedAvatars.has(id)) {
+        return "https://allmodeslist.pages.dev/defaultpfp.png";
+    }
+
     return `https://zirlaiexwekjusbhjibc.supabase.co/storage/v1/object/public/avatars/${id}.jpg`;
 }
 
 function handleAvatarError(img) {
+    const id = img.dataset.id;
+
+    failedAvatars.add(id);
+
     img.onerror = null;
     img.src = "https://allmodeslist.pages.dev/defaultpfp.png";
 }
@@ -73,3 +128,15 @@ function getRecords(records) {
 
     return total || records[0].mode_skill_pt || 0;
 }
+
+const sortingButtons = document.getElementById("sortingButtons");
+sortingButtons.addEventListener("click", (e) => {
+    if (e.target.tagName !== "BUTTON") return;
+
+    const buttons = sortingButtons.querySelectorAll("button");
+    buttons.forEach(btn => btn.classList.remove("active"));
+    e.target.classList.add("active");
+
+    const type = e.target.textContent;
+    displayLeaderboard(members, type);
+});
