707d4be95c174d5b82290b9541a26094dbacecd0
Atom <kevintran308@gmail.com>
2026-03-05 23:57:38 +0700

    blargh

--- scripts/get_docs.py
+++ scripts/get_docs.py
@@ -23,11 +23,6 @@ def get_credentials():
     return creds
   
 def read_paragraph_element(element):
-  """Returns the text in the given ParagraphElement.
-
-  Args:
-      element: a ParagraphElement from a Google Doc.
-  """
   text_run = element.get('textRun')
   if not text_run:
     return ''
@@ -35,12 +30,6 @@ def read_paragraph_element(element):
 
 
 def read_structural_elements(elements):
-  """Recurses through a list of Structural Elements to read a document's text
-  where text may be in nested elements.
-
-  Args:
-      elements: a list of Structural Elements.
-  """
   text = ''
   for value in elements:
     if 'paragraph' in value:
